3c0827768a6b7ef55e17e24e823b3a40f4c95874
Pliffisson <pliffissongomes@gmail.com>
2020-04-10 23:21:44 -0400

    Criando a interface de consultas

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
-import React from "react";
+import React, { Component } from "react";
 import MenuSuperior from "./components/Menu/MenuSuperior";
 import Resumo from "./components/Resumo/Resumo";
+import Consultas from "./components/Consultas/Consultas";
 
-export default function App() {
-  return (
-    <div>
-      <MenuSuperior />
-      <Resumo />
-    </div>
-  );
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <MenuSuperior />
+        <Resumo />
+        <Consultas />
+      </div>
+    );
+  }
 }
